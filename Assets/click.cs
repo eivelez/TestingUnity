1fcab3c00cfc672fa9fece4c0c06be2ccf95e327
Emilio Vélez <eivelez@miuandes.cl>
2020-10-05 13:25:21 -0300

    First Commit
    First commit

---
+++ Assets/click.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class click : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject  canvas;
+    TMPro.TextMeshProUGUI texto;
+    void Start()
+    {
+        canvas = this.transform.GetChild(0).gameObject;
+        texto = canvas.GetComponent<TMPro.TextMeshProUGUI>();
+        texto.text = "hola";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        Behaviour halo  = (Behaviour)GetComponent("Halo");
+        if(halo.enabled)
+        {
+            halo.enabled = false;
+        }
+        else
+        {
+            halo.enabled = true;
+        }
+        
+    }
+}
