580ec1071c22629e43a93ca09205c71af3ac76e3
Emilio Vélez <eivelez@miuandes.cl>
2020-10-05 14:57:44 -0300

    Scene created
    Hola FJJI

--- Assets/click.cs
+++ Assets/click.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class click : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject  canvas;
-    TMPro.TextMeshProUGUI texto;
     void Start()
     {
-        canvas = this.transform.GetChild(0).gameObject;
-        texto = canvas.GetComponent<TMPro.TextMeshProUGUI>();
-        texto.text = "hola";
         
     }
 
